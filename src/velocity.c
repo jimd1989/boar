04459c0e757a5ce4343ac2b10d181fececf2bc2c
jimd1989 <jamesdalrymple@protonmail.com>
2020-05-28 17:34:40 +0900

    Removed leftover synonyms.h cruft

--- src/velocity.c
+++ src/velocity.c
@@ -6,10 +6,8 @@
 #include "numerical.h"
 #include "wave.h"
 
-static float getVelocity(const uint16_t note);
-
-static float
-getVelocity(const uint16_t note) {
+float
+applyVelocityCurve(const Wave *vw, const uint16_t note) {
 
 /* Note is a set of two bytes. The lower byte is a value between 0 ... 127
  * representing the actual MIDI note, and the upper byte is a value between
@@ -19,13 +17,7 @@ getVelocity(const uint16_t note) {
  * touch-sensitive parameters. Note is shifted to the right by 9 places
  * intead of the usual 8, since its value cannot exceed 127. */
 
-  return (float)(note >> 9) / (float)MAX_MIDI_VALUE;
-}
-
-float
-applyVelocityCurve(const Wave *vw, const uint16_t note) {
-
-  const float vel = getVelocity(note);
+  const float vel = (float)(note >> 9) / (float)MAX_MIDI_VALUE;
 
   return interpolateCycle(vw, truncateFloat(vel, 1.0f));
 }
