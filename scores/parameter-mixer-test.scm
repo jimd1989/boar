1bb476ebab8eb882dec3848cbfc48de3cb02e2ca
jimd1989 <jamesdalrymple@protonmail.com>
2026-04-06 08:34:02 +1000

    mixer refactor

--- scores/parameter-mixer-test.scm
+++ scores/parameter-mixer-test.scm
@@ -4,20 +4,20 @@
 (audio-start! SIO-0)
 
 ; 2 outputs, 2 inputs, buffer len 1184
-(define mix (mixer-new-from-lengths 2 2 1184))
-(mixer-new-master-volume-set!   mix     0.3)
-(mixer-new-master-balance-set!  mix 0   1.0)
-(mixer-new-master-balance-set!  mix 1   1.0)
+(define mix (mixer-from-lengths 2 2 1184))
+(mixer-master-volume-set!   mix     0.3)
+(mixer-master-balance-set!  mix 0   1.0)
+(mixer-master-balance-set!  mix 1   1.0)
 
 ; input channel 1 → left ear
-(mixer-new-channel-volume-set!  mix 0   1.0)
-(mixer-new-channel-balance-set! mix 0 0 1.0)
-(mixer-new-channel-balance-set! mix 0 1 0.0)
+(mixer-channel-volume-set!  mix 0   1.0)
+(mixer-channel-balance-set! mix 0 0 1.0)
+(mixer-channel-balance-set! mix 0 1 0.0)
 
 ; input channel 2 → right ear
-(mixer-new-channel-volume-set!  mix 1   1.0)
-(mixer-new-channel-balance-set! mix 1 0 0.0)
-(mixer-new-channel-balance-set! mix 1 1 1.0)
+(mixer-channel-volume-set!  mix 1   1.0)
+(mixer-channel-balance-set! mix 1 0 0.0)
+(mixer-channel-balance-set! mix 1 1 1.0)
 
 ; audio handle state = mixer
 (audio-data-set! SIO-0 mix)
@@ -31,7 +31,7 @@
 
 ; scratch
 (osc-freq-set! o2 220.0)
-(f32slice-vector (mixer-new-channel-slice mix 0))
+(f32slice-vector (mixer-channel-slice mix 0))
 ; 1 + out-ch is incorrect
 ; should be... (*
 ;                 (+ 1 out-ch) ← vol    bal_0 … bal_n
@@ -42,8 +42,8 @@
 (audio-f-set! SIO-0
   (lambda (u8 x n)
     (if (> n 0)
-      (let ((ch1-sl (mixer-new-channel-slice mix 0))
-            (ch2-sl (mixer-new-channel-slice mix 1)))
+      (let ((ch1-sl (mixer-channel-slice mix 0))
+            (ch2-sl (mixer-channel-slice mix 1)))
         ; generate noise
         (fill-white-noise! no)
         ;(f32slice-copy-from-f32vector ch1-sl (noise-white-f32 no))
@@ -53,33 +53,33 @@
         (osc-fill-slice! o2 ch1-sl)
 
         ; mixdown
-        (mixer-new-mix! x u8)))))
+        (mixer-mix! x u8)))))
 
 (##sys#gc)
 
 ; OLD
 ; (import boar-param boar-mixer boar-slice srfi-4 srfi-18 (chicken string))
 ; 
-; (define mix (mixer-new-from-lengths 2 2 4))
-; (mixer-new-master-volume-set!   mix     111.0)
-; (mixer-new-master-balance-set!  mix 0   222.0)
-; (mixer-new-master-balance-set!  mix 1   333.0)
-; (mixer-new-channel-volume-set!  mix 0   444.0)
-; (mixer-new-channel-volume-set!  mix 1   777.0)
-; (mixer-new-channel-balance-set! mix 0 0 555.0)
-; (mixer-new-channel-balance-set! mix 0 1 666.0)
-; (mixer-new-channel-balance-set! mix 1 0 888.0)
-; (mixer-new-channel-balance-set! mix 1 1 999.0)
-; (params-new (mixer-new-params mix))
+; (define mix (mixer-from-lengths 2 2 4))
+; (mixer-master-volume-set!   mix     111.0)
+; (mixer-master-balance-set!  mix 0   222.0)
+; (mixer-master-balance-set!  mix 1   333.0)
+; (mixer-channel-volume-set!  mix 0   444.0)
+; (mixer-channel-volume-set!  mix 1   777.0)
+; (mixer-channel-balance-set! mix 0 0 555.0)
+; (mixer-channel-balance-set! mix 0 1 666.0)
+; (mixer-channel-balance-set! mix 1 0 888.0)
+; (mixer-channel-balance-set! mix 1 1 999.0)
+; (params-new (mixer-params mix))
 ; (define output (make-u8vector 16 0 #t #f))
-; (params-updated? (mixer-new-params mix))
-; (mixer-new-mix! mix output)
-; (mixer-new-audio mix)
-; (vector-ref (mixer-new-slices mix) 0)
-; (f32vector-length (mixer-new-audio mix))
-; (f32slice-to-write (mixer-new-master-slice mix))
-; (f32slice-written (mixer-new-channel-slice mix 0))
-; (f32slice-written (mixer-new-channel-slice mix 1))
+; (params-updated? (mixer-params mix))
+; (mixer-mix! mix output)
+; (mixer-audio mix)
+; (vector-ref (mixer-slices mix) 0)
+; (f32vector-length (mixer-audio mix))
+; (f32slice-to-write (mixer-master-slice mix))
+; (f32slice-written (mixer-channel-slice mix 0))
+; (f32slice-written (mixer-channel-slice mix 1))
 ; mix
 
 (import boar-param boar-mixer boar-noise boar-slice boar-osc srfi-4 srfi-18)
